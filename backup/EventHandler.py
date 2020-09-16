8a640dd628c0d3da76533fdc4f572a8ae3be7cb3
Jaimedgp <jaime.diez.gp@gmail.com>
2020-09-16 11:59:14 +0200

    Redo code reading watchdog docu

--- backup/EventHandler.py
+++ backup/EventHandler.py
@@ -1,38 +1,69 @@
-from watchdog.observers import Observer
 import time
-from watchdog.events import FileSystemEventHandler
 import os
-import json
-from shutil import copyfile
+
+from shutil import copyfile, move
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 
 class MyHandler(FileSystemEventHandler):
     """ Handler class to handle event """
 
     i = 1
+
     def __init__(self):
-        self.old_files_to_track = os.listdir("/home/jaimedgp/Documents/track")
+
+        self.old_files_to_track = os.listdir("/home/jaimedgp/Documents/track/")
 
     def on_modified(self, event):
         """ act when modified is detected """
 
-        for filename in os.listdir(folder_to_track):
-            print(filename)
-            if filename not in self.old_files_to_track:
-                src = folder_to_track + "/" + filename
-                new_destination = folder_destination + "/" + filename
+        # for filename in os.listdir(folder_to_track):
+        #    if filename not in self.old_files_to_track:
+        #        src = folder_to_track + "/" + filename
+        #        new_destination = folder_destination + "/" + filename
+
+        #        if os.path.isfile(src):
+        #            copyfile(src, new_destination)
+        #        if os.path.isdir(src):
+        #            os.mkdir(new_destination)
+    # self.old_files_to_track = os.listdir("/home/jaimedgp/Documents/track/")
+
+    def on_created(self, event):
+
+        print("created: ", event.src_path)
+
+        dest = event.src_path.replace(folder_to_track,
+                                                 folder_destination)
+
+        copyfile(event.src_path, dest)
 
 
-                if os.path.isfile(src):
-                    copyfile(src, new_destination)
-                if os.path.isdir(src):
-                    os.mkdir(new_destination)
+    def on_moved(self, event):
 
-        self.old_files_to_track = os.listdir("/home/jaimedgp/Documents/track")
+        print("Moved: ", event.src_path)
+
+        src = event.src_path.replace(folder_to_track,
+                                     folder_destination)
+        dest = event.dest_path.replace(folder_to_track,
+                                       folder_destination)
+
+        move(src, dest)
 
 
-folder_to_track = "/home/jaimedgp/Documents/track"
-folder_destination = "/home/jaimedgp/Documents/destination"
+    def on_deleted(self, event):
+
+        print("Deleted: ", event.src_path)
+
+        dest = event.src_path.replace(folder_to_track,
+                                      folder_destination)
+        os.remove(dest)
+
+
+folder_to_track = "/home/jaimedgp/Documents/track/"
+folder_destination = "/home/jaimedgp/Documents/destination/"
+
 event_handler = MyHandler()
 observer = Observer()
 observer.schedule(event_handler, folder_to_track, recursive=True)
@@ -43,4 +74,5 @@ try:
         time.sleep(10)
 except KeyboardInterrupt:
     observer.stop()
+
 observer.join()
